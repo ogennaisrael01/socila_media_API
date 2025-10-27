9f20f92ce200feb1643143a23d2ea997bc573bbc
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-10-27 17:36:43 +0100

    add vercel file: prepare for deployment

--- app/main.py
+++ app/main.py
@@ -2,16 +2,12 @@ from fastapi import FastAPI, status
 from app.routes import user_route, profile_route, following_route, post_route, likes_route, comment_route
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
 import os
 
 app = FastAPI()
 
 # Path to the favicon.ico file
 favicon_path = os.path.join(os.path.dirname(__file__), "static", "favicon.ico")
-
-# Mount the "static" directory
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/favicon.ico", include_in_schema=False)
 async def favicon():
